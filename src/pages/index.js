a65bb2f7eb443071295faf5ac7944cbcfc341f8e
Henri Brestea <henri.bresteau@gmail.com>
2021-07-19 11:23:28 +0200

    add InfoSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
@@ -14,7 +15,8 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle}></Sidebar>
       <Navbar toggle={toggle}></Navbar>
-      <HeroSection ></HeroSection>
+      <HeroSection></HeroSection>
+      <InfoSection></InfoSection>
     </>
   );
 };
