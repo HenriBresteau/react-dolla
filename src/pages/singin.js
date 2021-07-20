bc741bc449a0e0925a46dab7d77a70fafc196c40
Henri Brestea <henri.bresteau@gmail.com>
2021-07-20 14:10:53 +0200

    Signin Component  + style

--- src/pages/singin.js
+++ src/pages/singin.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Signin from "../components/Signin";
 
 function SigninPage() {
   return (
-    <div>
-      <h1>SIgn in page</h1>
-    </div>
+    <>
+      <Signin></Signin>
+    </>
   );
 }
 
